14e0965fdfb581cacf1f48870a242500e09eeb42
mpus <mateuszpodmokly@outlook.com>
2026-01-21 01:01:07 +0100

    continuous distributions begin

--- graphs.py
+++ graphs.py
@@ -3,38 +3,17 @@ import matplotlib.pyplot as plt
 from scipy.stats import poisson
 
 
-n = 21
-x = np.arange(0, n)
+plt.plot([0, 4], [0.25, 0.25])
+plt.scatter([0, 4], [0.25, 0.25])
 
-l = 1
-prob = poisson.pmf(x, l)
+plt.plot([-3, 0], [0, 0], c="C0")
+plt.plot([4, 7], [0, 0], c="C0")
+plt.xlim(-2, 6)
 
-for i in range(n):
-    if i == 0:
-        plt.scatter(x[i], prob[i], c="C0", label=r"$\lambda=1$")
-    else:
-        plt.scatter(x[i], prob[i], c="C0")
-
-l = 4
-prob = poisson.pmf(x, l)
-
-for i in range(n):
-    if i == 0:
-        plt.scatter(x[i], prob[i], c="red", label=r"$\lambda=4$")
-    else:
-        plt.scatter(x[i], prob[i], c="red")
-
-l = 10
-prob = poisson.pmf(x, l)
-
-for i in range(n):
-    if i == 0:
-        plt.scatter(x[i], prob[i], c="green", label=r"$\lambda=10$")
-    else:
-        plt.scatter(x[i], prob[i], c="green")
+# bottom = 
+plt.ylim(plt.ylim()[0], 0.4)
 
 plt.xlabel("X")
 plt.ylabel("p(x)")
 plt.title("Funkcja prawdopodobieństwa")
-plt.legend()
 plt.show()
