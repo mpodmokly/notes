dbc44ab1452b8073ffa3f9dfce64b57ceadfc503
mpus <mateuszpodmokly@outlook.com>
2026-01-31 20:30:47 +0100

    confidence intervals for mean

--- graphs.py
+++ graphs.py
@@ -1,22 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import norm
+from scipy.stats import t
 
 
-x = np.linspace(-3, 5, 1000)
-sigma = 1
+x = np.linspace(-5, 5, 1000)
 
-mu = 0
-y = norm.pdf(x, loc=mu, scale=sigma)
-plt.plot(x, y, c="C0", label=r"$\mu=0,\sigma=1$")
+nu = 1
+y = t.pdf(x, df=nu)
+plt.plot(x, y, c="C0", label=r"$\nu=1$")
 
-mu = 1
-y = norm.pdf(x, loc=mu, scale=sigma)
-plt.plot(x, y, c="red", label=r"$\mu=1,\sigma=1$")
+nu = 5
+y = t.pdf(x, df=nu)
+plt.plot(x, y, c="red", label=r"$\nu=5$")
 
-mu = 2
-y = norm.pdf(x, loc=mu, scale=sigma)
-plt.plot(x, y, c="green", label=r"$\mu=2,\sigma=1$")
+nu = 10
+y = t.pdf(x, df=nu)
+plt.plot(x, y, c="green", label=r"$\nu=10$")
 
 plt.legend()
 plt.xlabel(r"$X$")
